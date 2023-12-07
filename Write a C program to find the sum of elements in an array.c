3057245a1fcffe5d3cdd3e64b31df1694b2f2652
Arun Sharma <146552177+arun1645@users.noreply.github.com>
2023-12-07 21:32:29 +0530

    Add files via upload

---
+++ Write a C program to find the sum of elements in an array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements of the array: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum of elements in the array is: %d\n", sum);
+
+ return 0;
+}
